36a0ce5f97eedba2c8c7d237bb5dd4467f4d539f
Arun Prakash <arunslb123@gmail.com>
2016-05-02 19:02:16 +1000

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     $host = "us-cdbr-azure-southcentral-e.cloudapp.net";
     $user = "b5e6932691e7ad";
     $pwd = "431dfc7a";
-    $db = "acsm_4729051611eebc5";
+    $db = "acsm_4ea83fc2f007f8b";
     // Connect to database.
     try {
         $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
