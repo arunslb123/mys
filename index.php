c45fe847a2d5449b5e07294fe990fb578ec26b0f
Arun Prakash <arunslb123@gmail.com>
2016-05-02 14:39:02 +1000

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 <h1>Register here!</h1>
-<p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+<p>Fill in your name and email address check, then click <strong>Submit</strong> to register.</p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
